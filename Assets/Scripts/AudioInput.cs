1bf5ff79b44fe82cebad826670444fac66969c3a
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2017-01-21 04:42:12 +0100

    Added Microphone scene. Added AudioProcessor, which distinguishes between single noises and continued ones.

--- Assets/Scripts/AudioInput.cs
+++ Assets/Scripts/AudioInput.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class AudioInput : MonoBehaviour {
 
+	delegate void AudioInputReceived(float volume);
+	AudioInputReceived audioInputReceived;
+
 	AudioSource _audio;
+	public float lastInput;
 
 	public class Volume {
 		public const float ZERO   = 0.0f;
+		public const float TEST   = 0.005f;
 		public const float LOW    = 0.02f;
 		public const float MEDIUM = 0.05f;
 		public const float HIGH   = 0.08f;
@@ -20,15 +25,29 @@ public class AudioInput : MonoBehaviour {
 		_audio.loop = true;
 		//_audio.mute = true;
 		while (!(Microphone.GetPosition(null) > 0)){}
-		_audio.Play(); // Play the audio source!
+		_audio.Play();
+		audioInputReceived += TestLogVolume;
+		lastInput = 0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		float volume = GetAveragedVolume ();
-		volume = GetVolumeCardinal (volume);
-		if (volume > Volume.ZERO) {
-			Debug.Log (volume);
+		//if (volume >= 0.02f)
+			lastInput = volume;
+//		else
+//			lastInput = 0f;
+//		volume = GetVolumeCardinal (volume);
+//		if (volume > Volume.ZERO) {
+//			audioInputReceived (volume);
+//		}
+	}
+
+	void GetData() {
+		float[] data = new float[256];
+		_audio.GetOutputData (data, 0);
+		foreach (float s in data) {
+			Debug.Log (s);
 		}
 	}
 
@@ -49,7 +68,13 @@ public class AudioInput : MonoBehaviour {
 			return Volume.MEDIUM;
 		} else if (volume >= Volume.LOW) {
 			return Volume.LOW;
+		} else if (volume >= Volume.TEST) {
+			return Volume.TEST;
 		}
 		return Volume.ZERO;
+	}
+
+	void TestLogVolume(float volume) {
+		Debug.Log (volume);
 	}
 }
---
+++ Assets/Scripts/AudioProcessor.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioProcessor : MonoBehaviour {
+
+	class Volume {
+		public const float NONE = 0.0f;
+		public const float LOW = 0.02f;
+		public const float MIDDLE = 0.05f;
+		public const float HIGH = 0.08f;
+		public const float FLOW = -1f;
+	}
+
+	const int THRESHOLD_SINGLE = 7;
+
+	AudioInput _audioInput;
+
+	float _currentVolume; // = Volume.NONE;
+	int _sequenceCount; // = 0;
+	float _toTrigger; // = Volume.NONE;
+
+	void Start() {
+		_audioInput = GetComponent<AudioInput> ();
+		_currentVolume = Volume.NONE;
+		_sequenceCount = 0;
+		_toTrigger = Volume.NONE;
+	}
+
+	void Update() {
+		UpdateVolumeState (_audioInput.lastInput);
+	}
+
+	float GetVolumeCardinal(float volume) {
+		if (volume >= Volume.HIGH) {
+			return Volume.HIGH;
+		} 
+		else if (volume >= Volume.MIDDLE) {
+			return Volume.MIDDLE;
+		}
+		else if (volume >= Volume.LOW) {
+			return Volume.LOW;
+		}
+		return Volume.NONE;
+	}
+
+	void UpdateVolumeState(float volume) {
+		float newVolume = GetVolumeCardinal(volume);
+		if (newVolume == Volume.NONE) {
+			if (_toTrigger != Volume.NONE && _toTrigger != Volume.FLOW) {
+				TriggerSingle (_toTrigger);
+			}
+			if (_toTrigger != Volume.NONE) {
+				_toTrigger = Volume.NONE;
+			}
+		}
+		else if (newVolume < _currentVolume) {
+			if (_toTrigger == Volume.NONE) {
+				_toTrigger = _currentVolume;
+			}
+			else if (_toTrigger == Volume.FLOW) {
+				_sequenceCount = 0;
+			}
+		}
+		else if (newVolume == _currentVolume) {
+			if (_sequenceCount > THRESHOLD_SINGLE) {
+				_toTrigger = Volume.FLOW;
+				TriggerFlow (_currentVolume);
+			}
+			else {
+				_sequenceCount++;
+			}
+		}
+		else if (newVolume > _currentVolume) {
+			_sequenceCount = 0;
+		}
+		_currentVolume = volume;
+	}
+
+	void TriggerSingle(float volume) {
+		Debug.Log ("Single => " + GetVolumeText(volume));
+	}
+
+	void TriggerFlow(float volume) {
+		Debug.Log ("Flow => " + GetVolumeText (volume));
+	}
+
+	string GetVolumeText(float volume) {
+		if (volume >= Volume.HIGH) {
+			return "HIGH";
+		}
+		else if (volume >= Volume.MIDDLE) {
+			return "MIDDLE";
+		}
+		else if (volume >= Volume.LOW) {
+			return "LOW";
+		}
+		return "NONE";
+	}
+}
