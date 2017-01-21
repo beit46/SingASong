083bf61c14c67068020504fa262dfe7040d5b436
Enrico Bottani <enrico@heartquake.co>
2017-01-21 10:33:55 +0100

    Feature: Added line selection, we can now move objects from left to right. Add lines, layout dyanic according to screen size

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour {
 	public GameObject projectilePrefab;
-	
+
+	private LineSelector lineSelector;
+
+	void Awake() {
+		this.lineSelector = GetComponent<LineSelector>();
+	}
+
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.A)) {
@@ -18,6 +24,15 @@ public class Gun : MonoBehaviour {
 		if (Input.GetKeyDown(KeyCode.D)) {
 			Projectile projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity).GetComponent<Projectile>();
 			projectile.Shot(Vector2.up, PROJECTILE_TYPE.STRONG);
+		}
+
+
+		if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+			this.lineSelector.MoveLeft();
+		}
+
+		if (Input.GetKeyDown(KeyCode.RightArrow)) {
+			this.lineSelector.MoveRight();
 		}
 	}
 }
---
+++ Assets/Scripts/LineSelector.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineSelector : MonoBehaviour {
+	public List<Transform> lines = new List<Transform>();
+	public Transform currentLine = null;
+
+	public void MoveLeft() {
+		Debug.Log("move left");
+		int index = lines.IndexOf(currentLine);
+		if(index >= 1) {
+			currentLine = lines[index - 1];
+			MoveToLine(index);
+		}
+	}
+
+	public void MoveRight() {
+		Debug.Log("move right");
+		int index = lines.IndexOf(currentLine);
+		if(index <= 1) {
+			currentLine = lines[index + 1];
+			MoveToLine(index);
+		}
+	}
+
+	void MoveToLine(int lineIndex) {
+		this.transform.position = new Vector3(currentLine.position.x, this.transform.position.y, 0);
+	}
+}
