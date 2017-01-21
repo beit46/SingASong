a3eee25b4cf0552b221f590d717d08d975628394
Enrico Bottani <enrico@heartquake.co>
2017-01-21 10:50:20 +0100

    Feature: Moving the platform left and right

--- Assets/Scripts/TargetSpawner.cs
+++ Assets/Scripts/TargetSpawner.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TargetSpawner : MonoBehaviour {
+	private LineSelector lineSelector;
 	public float targetSpeed = 15.0f;
 	public List<Target> liveTargets;
 	public GameObject TargetPrefab;
 	bool isSpawning = false;
+
+	void Awake() {
+		lineSelector = GetComponent<LineSelector>();
+	}
 
 	void Update () {
 		if (liveTargets.Count <= 1 && !isSpawning) {
@@ -16,6 +21,12 @@ public class TargetSpawner : MonoBehaviour {
 	}
 
 	void SpawnTarget() {
+		int moveTo = Random.Range(0, 3);
+		if(moveTo == 0)
+			lineSelector.MoveLeft();
+		else if(moveTo == 2)
+			lineSelector.MoveRight();
+		
 		Target target = Instantiate(TargetPrefab, this.transform.position, Quaternion.identity).GetComponent<Target>();
 		target.Shot(Vector2.down, targetSpeed, (TARGET_TYPE)Random.Range(1, 4));
 		target.OnTargetDestroyed += TargetDestroyed;
