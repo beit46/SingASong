b2b3cc616cf94501079da1cbad70ebadd814b611
Enrico Bottani <enrico@heartquake.co>
2017-01-22 16:28:53 +0100

    Feature: 150BPM song events set

--- Assets/Scripts/ModeSelection.cs
+++ Assets/Scripts/ModeSelection.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using SonicBloom.Koreo;
 using SonicBloom.Koreo.Players;
+using UnityEngine.SceneManagement;
 
 public class ModeSelection : MonoBehaviour {
 	public SimpleMusicPlayer simpleMusicPlayer;
@@ -10,9 +11,26 @@ public class ModeSelection : MonoBehaviour {
 	public Koreography BPM_150_Koreography;
 	public TargetSpawner targetSpawner;
 
+
+	public void KoreographyEventCallback(KoreographyEvent koreographyEvent) {
+		//		Debug.Log("I got payload " + koreographyEvent.GetIntValue());
+		TargetSpawner.KOREO_EVENT_TYPE koreoEvent = (TargetSpawner.KOREO_EVENT_TYPE)koreographyEvent.GetIntValue();
+		switch(koreoEvent) {
+		case TargetSpawner.KOREO_EVENT_TYPE.SONG_END:
+				SceneManager.LoadScene ("MainMenu");
+			break;
+		default:
+			break;
+		}
+	}
+
 	// Use this for initialization
 	void Awake () {
-		bool isEasyModeEnabled = false;
+		Koreographer.Instance.RegisterForEvents("NewKoreographyTrack", KoreographyEventCallback);
+
+		string difficulty = PlayerPrefs.GetString ("Track");
+
+		bool isEasyModeEnabled = difficulty == "easy";
 		if(isEasyModeEnabled) {
 			this.simpleMusicPlayer.LoadSong(BPM_120_Koreography);
 			targetSpawner.targetSpeed = 5.0f;
