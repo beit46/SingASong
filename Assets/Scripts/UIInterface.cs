6894b405f1871a07043af3ce1ca0935493ec2376
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2017-01-22 10:25:32 +0100

    Changes projectile sprites, vertical scores and lives, tutorial with only one continued test.

--- Assets/Scripts/UIInterface.cs
+++ Assets/Scripts/UIInterface.cs
@@ -9,7 +9,8 @@ public class UIInterface : MonoBehaviour {
 	public Text lives;
 
 	public void SetScore(float score) {
-		this.score.text = "SCORE: " + (int)score;
+		//this.score.text = "SCORE: " + (int)score;
+		this.score.text = "S\nC\nO\nR\nE\n" + VerticalizeString(score.ToString());
 	}
 
 	public void SetMultiplier(float multiplier) {
@@ -18,6 +19,15 @@ public class UIInterface : MonoBehaviour {
 	}
 
 	public void SetLives(int lives) {
-		this.lives.text = "LIVES: " + lives;
+		//this.lives.text = "LIVES: " + lives;
+		this.lives.text = "L\nI\nV\nE\nS\n" + VerticalizeString(lives.ToString());
+	}
+
+	string VerticalizeString(string s) {
+		string vs = "";
+		foreach (char c in s) {
+			vs += "\n" + c;
+		}
+		return vs;
 	}
 }
